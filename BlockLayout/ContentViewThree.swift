ba620ff8b4ed0615538cdcd7df655faf62f5bbdc
Shiyi Li <sli@lcs.on.ca>
2026-02-06 11:15:54 -0500

    Completed my third layout

---
+++ BlockLayout/ContentViewThree.swift
@@ -0,0 +1,24 @@
+//
+//  ContentViewThree.swift
+//  BlockLayout
+//
+//  Created by Shiyi Li on 2026-02-06.
+//
+
+import SwiftUI
+
+struct ContentViewThree: View {
+    var body: some View {
+        VStack{
+            HStack{
+                Rectangle()
+                Rectangle()
+            }
+            Rectangle()
+        }
+    }
+}
+
+#Preview {
+    ContentViewThree()
+}
