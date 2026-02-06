516978e694adf084f4753b2465c84b510ef083c2
Shiyi Li <sli@lcs.on.ca>
2026-02-06 09:09:25 -0500

    Initial Commit

---
+++ BlockLayout/BlockLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockLayoutApp.swift
+//  BlockLayout
+//
+//  Created by Shiyi Li on 2026-02-06.
+//
+
+import SwiftUI
+
+@main
+struct BlockLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlockLayout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlockLayout
+//
+//  Created by Shiyi Li on 2026-02-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
